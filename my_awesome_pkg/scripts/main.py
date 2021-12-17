eb13f68f0e29339c2a95a83fba03c0f6939bc4c4
G-Chervier <guillaume.chervier@etu.imt-nord-europe.fr>
2021-12-17 15:06:08 +0100

    simple turn when obstacle

--- my_awesome_pkg/scripts/main.py
+++ my_awesome_pkg/scripts/main.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+import random 
 import math, rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+
 # Initialize ROS::node
 rospy.init_node('move', anonymous=True)
+INITIAL_SPEED = 1
+MAX_SPEED = 5
+
+vitesse = INITIAL_SPEED
+acceleration = 0.1
+isTurning = False
+turningCount = 0
+amountOfTurn = 0
 
 commandPublisher = rospy.Publisher(
     '/cmd_vel',
@@ -13,32 +23,44 @@ commandPublisher = rospy.Publisher(
 
 # Publish velocity commandes:
 def move_command(data):
+    global vitesse
     # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
     cmd= Twist()
-    cmd.linear.x= 1
+    cmd.linear.x= vitesse
     commandPublisher.publish(cmd)
-
 # call the move_command at a regular frequency:
 rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
-'''
+
 def callB(data):
-    obstacles= []
-    angle= data.angle_min
-    for aDistance in data.ranges :
-        if 0.1 < aDistance and aDistance < 5.0 :
-            aPoint= [math.cos(angle) * aDistance, math.sin( angle ) * aDistance]
-            obstacles.append( aPoint )
-        angle+= data.angle_increment
-    rospy.loginfo( str(
-        [ [ round(p[0], 2), round(p[1], 2) ] for p in  obstacles[0:10] ] 
-    ) + " ..." )
+    global vitesse, acceleration,INITIAL_SPEED, MAX_SPEED, isTurning, turningCount, amountOfTurn
+    sample = data.ranges[355:365]
 
-    cmd= Twist()
-    cmd.linear.x= 1 #add x, y, z angular 
-    commandPublisher.publish(cmd)
+    if min(sample) < 6:
+        if vitesse > 1:
+            vitesse -= acceleration
 
-rospy.Subscriber("/base_scan", LaserScan, callB )
-'''
+    if turningCount > amountOfTurn:
+        isTurning = False
+        turningCount = 0
+
+    if isTurning:
+        turningCount += 1
+        cmd= Twist()
+        cmd.angular.z= 1.5 #add x, y, z angular 
+        commandPublisher.publish(cmd)
+
+
+    if min(sample) < 2:
+        amountOfTurn = random.randint(1, 10)
+        vitesse = INITIAL_SPEED
+        isTurning = True
+
+    if vitesse < MAX_SPEED:
+        vitesse += acceleration
+
+
+rospy.Subscriber("/scan", LaserScan, callB )
+
 # spin() enter the program in a infinite loop
 print("Start move.py")
 rospy.spin()
