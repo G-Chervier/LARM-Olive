46c8f7f1707d07f455f08f79df74ffb352b812be
G-Chervier <guillaume.chervier@etu.imt-nord-europe.fr>
2022-01-12 10:16:59 +0100

    clean version

--- grp-olive/scripts/move-to.py
+++ grp-olive/scripts/move-to.py
@@ -1,72 +1,31 @@
-#!/usr/bin/python3
-import random
+#!/usr/bin/env python3
+from threading import local
 import rospy
-from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
+import tf
+from geometry_msgs.msg import *
 
 
-class Brain:
-    def __init__(self, verbose = True):
-        self.commandPublisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.cmd = Twist()
-        self.verbose = verbose
 
-        self.vitesse = 2
+class myNode:
+    def __init__(self):
+        rospy.init_node('move_to', anonymous=True)
+        self.tfListener = tf.TransformListener()
+        self.sub = rospy.Subscriber("/move_base_simple/goal", PoseStamped, self.callback)
+        self.pub = rospy.Publisher(
+            '/cmd_vel_mux/input/navi',
+            Twist, queue_size=10
+        )
+        rospy.Timer( rospy.Duration(0.1), self.callback2, oneshot=False )
+    def callback(self, goal):
+        rospy.loginfo("I Got a goal : ")
+        print(goal)
+        local_goal= self.tfListener.transformPose("/base_footprint", goal)
+        print(local_goal)
 
-        self.isTurning = False
-        self.turningCount = 0
-        self.maxTurning = 30
-
-        rospy.init_node('move', anonymous=True)
-        rospy.Subscriber("/scan", LaserScan, self.scanner)
-        rospy.Timer(rospy.Duration(0.1), self.run, oneshot=False )
+    def callback2(self,data):
+        #Do some work
+        a = data
     
-    def getROI(self, data):
-        return data.ranges[355:365]
 
-    def setAmountRotation(self):
-        self.maxTurning = random.randint(30, 60)
-        if(self.verbose):
-            print(f"amount of turn : {self.maxTurning}")
-
-
-    def shouldTurn(self, roi):
-        return min(roi) < 2
-
-    def handleTurning(self):
-        if(self.isTurning):
-            self.turningCount += 1
-            self.cmd.angular.z = 1
-
-        if(self.turningCount > self.maxTurning):
-            self.isTurning = False
-            self.cmd.angular.z = 0
-            self.turningCount = 0
-
-            if(self.verbose):
-                print("stop turning")
-
-
-    def scanner(self, data):
-        roi = self.getROI(data)
-
-        if(self.shouldTurn(roi) and not self.isTurning):
-            self.isTurning = True
-            self.setAmountRotation()
-            if(self.verbose):
-                print("start turning")
-
-        self.handleTurning()
-
-
-    def run(self, data):
-        self.cmd.linear.x = self.vitesse
-        if(self.isTurning):
-            self.cmd.linear.x = 0
-        self.commandPublisher.publish(self.cmd)
-
-
-brain = Brain()
-
-print("Start move.py")
+myNode()
 rospy.spin()
