35dcaf68fa7ea5a2a4cad4e11f1ffd7d862027ab
Gaétan <hgaga77@gmail.com>
2022-01-14 15:30:30 +0100

    changes: detectfaces.py

--- scripts vision/detectfaces.py
+++ scripts vision/detectfaces.py
@@ -1,21 +1,22 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
-classifierFace = cv2.CascadeClassifier('/home/gaetan/visionPython/classifier_training/Test_LBP/data/cascade.xml')
+classifierFace = cv2.CascadeClassifier('/home/gaetan/visionPython/classifier_training/ws/data/cascade.xml')
+#classifierFace = cv2.CascadeClassifier('/home/gaetan/catkin_ws/src/LARM-Olive/scripts vision/bottle.xml')
 
 while True:
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = classifierFace.detectMultiScale(gray, 1.3, 5)
+    faces = classifierFace.detectMultiScale(gray, 1.2, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
         roiGray = gray[y:y+h, x:x+w]
         roiColor = frame[y:y+h, x:x+w]
-
-    cv2.imshow('frame', frame)
+    
     cv2.imshow('bot view', gray)
+    cv2.imshow('frame', frame)
+    
     
     if cv2.waitKey(1)&0xFF==ord('q'):
         break
