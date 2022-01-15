c9ee1362dd37d717f0a3289ddebfc680b866e8be
Gaétan <hgaga77@gmail.com>
2022-01-15 18:56:48 +0100

    Visualization in Rviz OK

--- grp-olive/scripts/main.py
+++ grp-olive/scripts/main.py
@@ -1,44 +1,58 @@
+#!/usr/bin/env python3
+
 import rospy
 from visualization_msgs.msg import Marker
 from sensor_msgs.msg import Image
-from geometry_msgs.msg import PoseStamped
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import *
+from std_msgs.msg import ColorRGBA
 import cv2
 from cv_bridge import CvBridge
 import os
+import tf
 
 class MyNode:
     def __init__(self):
+        self.odom = rospy.Subscriber("/odom",Odometry,self.getPose)
         self.sub = rospy.Subscriber("camera/color/image_raw",Image,self.detection)
-        self.odom = rospy.Subscriber("/odom",PoseStamped,self.getPose)
         self.pub = rospy.Publisher(
             '/bottle',
-            Marker, queue_size=10
+            Marker, queue_size=5
         )
         dirname = os.path.dirname(__file__)
         filename = os.path.join(dirname, 'cascade.xml')
-        self.classifier = cv2.CascadeClassifier('cascade.xml')
+        self.classifier = cv2.CascadeClassifier(filename)
+        self.marker= Marker()
 
     def detection(self,img):
         bridge = CvBridge()
-        frame = bridge.imgmsg_to_cv2(img, desired_encoding='passthrough')
+        frame = bridge.imgmsg_to_cv2(img, desired_encoding='bgr8')
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = self.classifier.detectMultiScale(frame, 1.5, 5)
+        objs = self.classifier.detectMultiScale(frame, 1.05, 7)
 
-        for (x, y, w, h) in faces:
+        for (x, y, w, h) in objs:
+            print('I detect one')
             cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
-            roiGray = gray[y:y+h, x:x+w]
-            roiColor = frame[y:y+h, x:x+w]
-        #cv2.imshow('bot view', gray)
-        #cv2.imshow('frame', frame)
+            self.publish()
+        cv2.imshow('bot view', gray)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1)&0xFF==ord('q'):
             cv2.destroyAllWindows()
-    def publish(self):
-        cmd = Marker()
-        cmd.pose = self.pose
-        self.pub.publish(cmd)
+
 
     def getPose(self,data):
-        self.pose = data.pose
+        self.marker.pose = data.pose.pose
+
+    def publish(self):
+        cmd = Marker(
+                type=Marker.CUBE,
+                id=0,
+                lifetime=rospy.Duration(0.5),
+                pose=self.marker.pose,
+                scale=Vector3(0.1, 0.1, 0.1),
+                color=ColorRGBA(0.0, 1.0, 0.0, 0.8))
+        cmd.header.frame_id='map'
+        self.pub.publish(cmd)
 
 rospy.init_node('bottle_detector',anonymous=True)
 myNode = MyNode()
