bfcee6c86ab8cc9fe84dd5180b1a648a6c669f3c
Gaétan <hgaga77@gmail.com>
2022-01-21 12:14:12 +0100

    modif avoid_obstacles.py

--- grp-olive/scripts/avoid_obstacles.py
+++ grp-olive/scripts/avoid_obstacles.py
@@ -6,7 +6,7 @@ import numpy as np
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
-DIST_LASER_ROBOT = 0.5
+DIST_LASER_ROBOT = 0.7
 FRONT_ANGLE = 30
 isTurningr = False
 isTurningl = False
@@ -34,7 +34,7 @@ def turn_left():
         isTurningl = True
         countTurning +=1
         print("Turning left") 
-        vitessex = 0.05
+        vitessex = 0.1
         vitessez = radians(90)
 
 def turn_right():
@@ -43,13 +43,13 @@ def turn_right():
         countTurning +=1
         isTurningr
         print("Turning right")
-        vitessex = 0.05
+        vitessex = 0.1
         vitessez = -radians(90)
 
 def move_forward():
     global vitessex, vitessez
     print("Moving forward")
-    vitessex = 0.2
+    vitessex = 0.3
     vitessez = 0
 
 def myhook():
